3db8b1d688d48f144819c89b080bdcc28273666c
Romain Laurent <romain.laurent@cdiscount.com>
2023-07-21 13:47:37 +0200

    feat: 🎸 add MUI theme, add AppBar to Template

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,17 @@
-import { RouterProvider } from "react-router-dom";
 import "./app.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { router } from "./router.jsx";
+import { RouterProvider } from "react-router-dom";
+import { theme } from "./theme";
+import { ThemeProvider } from "@mui/material";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
--- src/Template/index.jsx
+++ src/Template/index.jsx
@@ -1,10 +1,66 @@
-import { Outlet } from "react-router-dom";
-import Header from "../Header";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  IconButton,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
+
+const mainMenu = [
+  { name: "Produits", url: "/" },
+  { name: "Commandes", url: "/orders" },
+  { name: "Finance", url: "/financial" },
+  { name: "Rapports", url: "/reports" },
+  { name: "Messages", url: "/messages" },
+  { name: "Marketplaces", url: "/marketplaces" },
+];
 
 export default function Template() {
   return (
     <>
-      <Header />
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              to="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Seller Catalog
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {mainMenu.map(({ name, url }) => (
+                <Button
+                  key={url}
+                  component={Link}
+                  to={url}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {name}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Sergio" src="/sergio.jpg" />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
       <Outlet />
     </>
   );
