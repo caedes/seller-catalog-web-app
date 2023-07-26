bba91f0d910cc67080cc5350a1697e37942d3a90
Romain Laurent <romain.laurent@cdiscount.com>
2023-07-26 12:43:48 +0200

    feat: 🎸 create the real ProductPage

--- src/ds/pages/Page/Page.jsx
+++ src/ds/pages/Page/Page.jsx
@@ -3,15 +3,15 @@ import { node, string } from "prop-types";
 
 export default function Page({ title, children }) {
   return (
-    <>
-      <Box sx={{ ml: 21, py: 8 }}>
+    <Box sx={{ px: 3 }} component="main">
+      <Box sx={{ ml: 21, py: 8 }} component="header">
         <Typography variant="h5" component="h2">
           {title}
         </Typography>
       </Box>
 
       {children}
-    </>
+    </Box>
   );
 }
 
--- src/products/ProductRoute/ProductRoute.jsx
+++ src/products/ProductRoute/ProductRoute.jsx
@@ -1,8 +1,89 @@
 import { useParams } from "react-router-dom";
 import { Page } from "../../ds/pages";
+import { useProduct } from "../../hooks";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  LinearProgress,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Edit, Favorite } from "@mui/icons-material";
+import { Price } from "../../ds/atoms";
+import ProductCondition from "../ProductCondition";
+
+const defaultRenderCell = ({ value }) => value;
 
 export default function ProductRoute() {
   const { id } = useParams();
+  const { isLoading, data: product } = useProduct({ id });
 
-  return <Page title={`Product: ${id}`} />;
+  if (isLoading) return <LinearProgress />;
+
+  const productProperties = [
+    { name: "Nom", value: product.name },
+    { name: "Prix", value: product.price, type: "number", renderCell: Price },
+    { name: "Stock", value: product.stock, type: "number" },
+    {
+      name: "Condition",
+      value: product.condition,
+      renderCell: ProductCondition,
+    },
+  ];
+
+  return (
+    <Page title={product.name}>
+      <Alert severity="success">
+        <AlertTitle>Votre fiche produit a été enrichie avec succès</AlertTitle>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus
+        temporibus deserunt adipisci nemo deleniti earum veritatis rem, dolores
+        asperiores velit facere eum dolore esse voluptates laudantium odit
+        commodi id!
+      </Alert>
+
+      <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1, my: 2 }}>
+        <Button variant="outlined" startIcon={<Favorite />}>
+          Ajouter aux favoris
+        </Button>
+        <Button startIcon={<Edit />}>Éditer</Button>
+      </Box>
+
+      <Box component="article" sx={{ display: "flex", gap: 2 }}>
+        <img
+          src={product.imageUrl}
+          alt={product.name}
+          width={300}
+          height={300}
+        />
+        <TableContainer>
+          <Typography variant="h6">Propriétés</Typography>
+          <Table aria-label="Fiche produit">
+            <TableBody>
+              {productProperties.map(
+                ({ name, value, type, renderCell = defaultRenderCell }) => (
+                  <TableRow key={name}>
+                    <TableCell component="th" scope="row">
+                      {name}
+                    </TableCell>
+                    <TableCell
+                      align={type === "number" ? "right" : "left"}
+                      sx={{ fontWeight: "bold" }}
+                    >
+                      {renderCell({ value })}
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Page>
+  );
 }
